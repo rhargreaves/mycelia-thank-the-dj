6f12fd59b5657791753791356ca6fe3fc9f74c5a
emashliles <emashliles@gmail.com>
2016-04-03 12:43:58 +0100

    Now displays radio station location on a google map embed. it doesn't do it with any node swag tho :'(

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var request = require('request');
 var _ = require('underscore');
+var Handlebars = require('handlebars');
 
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//app.engine('handlebars', exphbs.engine);
+
 var url = "http://search.radioplayer.co.uk/qp/v3/onair?rpIds=340,1026,1094,1336,1254,425,341,342,343,344,347,349";
 var requestedArtist = "No song entered please add a song";
 var successfulPlay = [];
+
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        link: function (stationName) {return new Handlebars.SafeString(
+    stationName.replace(' ', '%20')
+  );}
+    }
+});
+
+
+//var formatFunction = function('link', function(stationName) {
+  //stationName = Handlebars.escapeExpression(stationName);
+
+  //return new Handlebars.SafeString(
+    //stationName.replace(' ', '%20')
+  //);
+//});
+
+
 
 app.get('/', function (req, res) {	
 	getJsonFromJsonP(url, function(error, json){
@@ -27,10 +50,15 @@ app.get('/', function (req, res) {
 	var songToReport = cleanPlays[0];
 	
 	
-	res.render('home', {songName: songToReport.name, showName: songToReport.name, stationName:songToReport.serviceName});
+	
+	res.render('home', {songName: songToReport.name, showName: songToReport.name, stationName:songToReport.serviceName, helpers: {link: function (stationName) {return new Handlebars.SafeString(
+    stationName.replace(' ', '%20')
+  );}}});
 });
 
 app.listen(3000);
+
+
 
 var findSongPlay = function(stationResult){
 	var songPlaysFromResults = _.findWhere(stationResult,{ artistName: requestedArtist });
