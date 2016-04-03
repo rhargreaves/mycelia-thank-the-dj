27dc2981a636c8217e2e6963654ed8aca59338da
emashliles <emashliles@gmail.com>
2016-04-03 01:30:28 +0100

    it works sometimes now.

--- app.js
+++ app.js
@@ -10,24 +10,49 @@ app.set('view engine', 'handlebars');
 
 var url = "http://search.radioplayer.co.uk/qp/v3/onair?rpIds=340,1026,1094,1336,1254,425,341,342,343,344,347,349";
 var requestedArtist = "No song entered please add a song";
+var successfulPlay = [];
 
 app.get('/', function (req, res) {	
 	getJsonFromJsonP(url, function(error, json){
+	    successfulPlay = [];
 		requestedArtist = req.query.artistname;
 		console.log(requestedArtist);
 		_.each(json.results, findSongPlay);
 	});
 	
-	res.render('home');
+	var cleanPlays = successfulPlay;
+	
+	console.log(cleanPlays);
+	
+	var songToReport = cleanPlays[0];
+	
+	
+	res.render('home', {songName: songToReport.name, showName: songToReport.name, stationName:songToReport.serviceName});
 });
 
 app.listen(3000);
 
 var findSongPlay = function(stationResult){
-	var songPlaysFromResults = _.findWhere(stationResult,{song : true , artistName: requestedArtist });
-	console.log(songPlaysFromResults);		
+	var songPlaysFromResults = _.findWhere(stationResult,{ artistName: requestedArtist });
+
+	if(typeof songPlaysFromResults !== 'undefined' && songPlaysFromResults)
+	{
+		successfulPlay.push(songPlaysFromResults);
+	}
+	
+
 }
 
+
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
+}
 
 var getJsonFromJsonP = function (url, callback) {
 request(url, function (error, response, body) {
